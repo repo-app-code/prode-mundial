98a8e266a504f5b415cb9648785e72054ba25fee
Luciano Agustin Temudio <luciano.agustin.temudio@gmail.com>
2026-06-04 21:49:26 -0300

    pass reset nodemailer 3

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,7 +13,9 @@ function getBaseUrl() {
 
 function createMailer() {
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
   });
 }
